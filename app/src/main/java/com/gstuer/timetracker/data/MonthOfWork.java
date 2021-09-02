426076a246eb9491bd0d5df37216321867930560
Moritz Gstuer <moritz@gstuer.com>
2021-09-02 16:31:58 +0200

    Add class representing a whole month of work

---
+++ app/src/main/java/com/gstuer/timetracker/data/MonthOfWork.java
@@ -0,0 +1,45 @@
+package com.gstuer.timetracker.data;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class MonthOfWork {
+    private static final String JSON_ALIAS_YEAR = "year";
+    private static final String JSON_ALIAS_MONTH = "month";
+    private static final String JSON_ALIAS_ENTRIES = "entries";
+
+    @JsonProperty(JSON_ALIAS_YEAR) private final int year;
+    @JsonProperty(JSON_ALIAS_MONTH) private final int month;
+    @JsonProperty(JSON_ALIAS_ENTRIES) private final List<Entry> entries;
+
+    @JsonCreator
+    public MonthOfWork(@JsonProperty(JSON_ALIAS_YEAR) int year,
+                       @JsonProperty(JSON_ALIAS_MONTH) int month,
+                       @JsonProperty(JSON_ALIAS_ENTRIES) List<Entry> entries) {
+        this.year = year;
+        this.month = month;
+        this.entries = entries;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getMonth() {
+        return this.month;
+    }
+
+    public List<Entry> getEntries() {
+        return this.entries;
+    }
+
+    public void addEntry(Entry entry) {
+        this.entries.add(entry);
+    }
+
+    public void removeEntry(Entry entry) {
+        this.entries.remove(entry);
+    }
+}
